e357b7731e7ae2f802d7698dc1ccb277d2ec3988
Barkat0126 <barkat0126@gmail.com>
2026-01-09 19:49:36 +0530

    feat: configure Atlas connection and start script

--- server/index.js
+++ server/index.js
@@ -23,10 +23,6 @@ cronJobs.start();
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
-
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
