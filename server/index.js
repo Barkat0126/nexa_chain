5cdb63669bb5a9f54a7dea032e65eef1d257cb2d
Barkat0126 <barkat0126@gmail.com>
2026-01-09 21:18:30 +0530

    Enhance CORS config with methods and headers

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: ['https://nexa-chain.vercel.app', 'http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 
